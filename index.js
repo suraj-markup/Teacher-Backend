ebcbf7d486c58b0bb2e8a08c8ff864ce96261195
suraj_markup <96483690+suraj-markup@users.noreply.github.com>
2025-05-24 00:48:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
     origin: ['http://localhost:8080', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization','expires']
 }));
 app.use(express.json());
 

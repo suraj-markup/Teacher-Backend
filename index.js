f227bed39b246200431c63ffaee62dad5efdacfa
suraj_markup <96483690+suraj-markup@users.noreply.github.com>
2025-05-24 00:45:51 +0530

    Update allowed origins

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:8080', 'http://localhost:5173'
+    origin: ['http://localhost:8080', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
